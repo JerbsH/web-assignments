8da8613a4662e0f0d777835f286f9c93d605e39d
Jere <101744771+JerbsH@users.noreply.github.com>
2023-03-27 19:51:52 +0300

    fixes and stuff

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,22 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
+const { body } = require("express-validator");
 
 router
 	.route("/")
-	.get(userController.getUserList)
-	.post(userController.postUser)
+	.get(
+		body("name").isAlphanumeric().isLength({ min: 1, max: 100 }).escape().trim(),
+		body("email").isEmail(),
+		body("password").isLength({ min: 8 }),
+		userController.getUserList)
+	.post(
+		body("name").isAlphanumeric().isLength({ min: 1, max: 100 }).escape().trim(),
+		body("email").isEmail(),
+		body("password").isLength({ min: 8 }),
+		userController.postUser)
 	.put(userController.putUser);
 
 router
